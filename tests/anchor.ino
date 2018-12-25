5108930ab266b79d036f7a65aaf5e3e9b8644da4
탁동혁 <tdh8316@naver.com>
2018-12-25 10:25:14 +0900

    initial commit

--- tests/anchor.ino
+++ tests/anchor.ino
@@ -15,10 +15,9 @@ void setup() {
   DW1000Ranging.attachNewRange(newRange);
   DW1000Ranging.attachBlinkDevice(newBlink);
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
-  //Enable the filter to smooth the distance
-  //DW1000Ranging.useRangeFilter(true);
+  DW1000Ranging.useRangeFilter(true);
   
-  //we start the module as an anchor
+  // start the module as an anchor
   DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 }
 
