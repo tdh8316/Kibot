b802b3bd6fc02347e5dce12579173e803c96eb54
Donghyeok Tak <tdh8316@naver.com>
2019-01-11 15:29:08 +0900

    Update robot.ino

--- core/robot/robot.ino
+++ core/robot/robot.ino
@@ -2,11 +2,11 @@
   Copyright 2018-2019. Donghyeok Tak
 */
 
-#include <Arduino.h>
 #include <SoftwareSerial.h>
 
 #define Bluetooth_TX 2
 #define Bluetooth_RX 3
+
 
 SoftwareSerial Bluetooth(Bluetooth_TX, Bluetooth_RX);
 
@@ -19,12 +19,33 @@ size_t log(String s) {
 void setup() {
   Serial.begin(9600);
   Bluetooth.begin(9600);
+
+  // DC모터
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void moveForward() {
+  analogWrite(5, 0);
+  analogWrite(6, 150);
+  analogWrite(10, 0);
+  analogWrite(11, 150);
+}
+
+void moveStop() {
+  analogWrite(5, 0);
+  analogWrite(6, 0);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
 }
 
 
 void loop() {
   // 블루투스 신호 대기
   if (Bluetooth.available()) {
-    log(Bluetooth.parseInt());
+    log(String(Bluetooth.parseInt()));
+    moveForward();
   }
 }
