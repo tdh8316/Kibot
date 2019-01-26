86981018d80a0ef867ab8c5377d215fc8289be18
탁동혁 <tdh8316@naver.com>
2019-01-26 23:42:13 +0900

    Remove unused files

--- client/native/app/src/main/java/com/takturstudio/tdh8316/kibot/client/KibotConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.takturstudio.tdh8316.kibot.client
-
-const val KIBOT_BLUETOOTH_NAME: String = "Kibot"
-const val LOG_SPEECH_RECOGNIZE: String = "Speech Recognition"
-const val LOG_BLUETOOTH: String = "Bluetooth"
-
-const val PERMISSION_REQUEST_CODE: Int = 0
