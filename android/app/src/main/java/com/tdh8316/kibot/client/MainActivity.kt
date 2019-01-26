d53b021fadac2f78988f5847d09296f77745ba08
탁동혁 <tdh8316@naver.com>
2019-01-26 23:50:41 +0900

    Add initial client app

---
+++ android/app/src/main/java/com/tdh8316/kibot/client/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tdh8316.kibot.client
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
