ce87444de571b88cc96a8274c699e18f9caa88d0
Donghyeok Tak <tdh8316@naver.com>
2019-02-15 15:54:30 +0900

    Add a test for ranging

---
+++ tests/core_noble/core_noble.ino
@@ -0,0 +1,123 @@
+/*
+  Copyright 2018-2019. Donghyeok Tak
+  Required Arduino Mega 2560
+  Kibot Main Processing Unit
+*/
+
+#if !defined(ARDUINO_AVR_MEGA2560)
+#error "You must use the Arduino Mega 2560 board."
+#endif
+
+#define Bluetooth Serial1
+#define print Serial.print
+#define println Serial.println
+
+#define x_min range[0]
+#define x_max range[1]
+
+const int SERIAL_TIMEOUT = 100; // ms
+
+// On Arduino MEGA, the double implementation is exactly the same as the float, with no gain in precision.
+
+// Current position (x, y)
+float pos_x, pos_y = 0;
+
+// The range for the destination
+float range[2] = {0, 0};
+
+
+void setup() {
+  // Start the serial communication in 115200 bps for compatibility with DWM1000
+  Serial.begin(115200);
+  println("This program is compiled at " __DATE__ " " __TIME__ ".");
+  println("Starting Kibot...");
+
+  print("  Change Serial timeout to 100ms...");
+  Serial.setTimeout(SERIAL_TIMEOUT);
+  println("[Done]");
+
+  print("  Start Bluetooth...");
+  Bluetooth.begin(9600);
+  println("[Done]");
+
+  // Initialize DC motor
+  print("  Change pin mode used for DC motor...");
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  println("[Done]");
+
+  x_min = 1.5;
+  x_max = 2.5;
+
+  println("  Everything is initialized.");
+  println("The setup sequence is done. It took " + String(millis()) + "ms.\n");
+}
+
+
+void moveForward() {
+  println("moveForward");
+  analogWrite(5, 0);
+  analogWrite(6, 255);
+  analogWrite(10, 0);
+  analogWrite(11, 255);
+}
+
+void moveBack() {
+  println("movaBack");
+  analogWrite(5, 255);
+  analogWrite(6, 0);
+  analogWrite(10, 255);
+  analogWrite(11, 0);
+}
+
+void moveStop() {
+  println("moveStop");
+  analogWrite(5, 0);
+  analogWrite(6, 0);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
+}
+
+
+void setRange(int id = -1) {
+  switch (id) {
+    case 101:
+      x_min, x_max = 1.5, 2.5; break;
+
+    case -1:
+      x_min = x_max = 0; break;
+  }
+}
+
+
+void loop() {
+
+  // DWM1000 communicates with the Kibot in Serial.
+  if (Serial.available()) {
+    float x = Serial.parseFloat(); if (x != 0.0f) pos_x = x;
+    // Only 1-d guidance is implemented.
+  }
+
+  // Kibot client communicates with the Kibot in Bluetooth
+  if (Bluetooth.available())
+    setRange(Bluetooth.parseInt());
+
+  // If the range is invalid, it does not need to continue running
+  if (x_min == x_max)
+    return;
+
+  /* Logic:
+      1-d space: (x_min < pos_x && x_max >= pos_x) => in range
+      2-d space: ((x_min < pos_x && x_max >= pos_x) && (y_min < pos_y && y_max >= pos_y)) => in range
+  */
+
+
+  if (pos_x < x_min) moveForward();
+  else if (pos_x > x_max) moveBack();
+  else moveStop();
+  
+
+  delay(SERIAL_TIMEOUT);
+}
